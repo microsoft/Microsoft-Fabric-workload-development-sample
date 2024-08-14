599dbc4c1c38c6ff246d6b7771392b66d015cd89
Nadav Barzilai <nbarzilai@microsoft.com>
2024-08-14 09:43:22 +0300

    add color and build mode

--- Frontend/validation/validateSchema.js
+++ Frontend/validation/validateSchema.js
@@ -19,13 +19,16 @@ const ROOT_DIR = './Package/';
 const validateItem = new Ajv().compile(itemSchema);
 const validateProduct = new Ajv().compile(productSchema);
 
+// Red color place holder (and back to normal)
+const errorColor = "'\x1b[31m%s\x1b[0m'"; 
+
 const itemDataMap = new Map();
 // Load data
 const productData = require('./../Package/Product.json');
 
 // Validate Product
 if (!validateProduct(productData)) {
-  console.error('Product data is invalid:', validateProduct.errors);
+  console.error(errorColor, 'Product data is invalid:', validateProduct.errors);
   process.exit(1);
 }
 
@@ -54,7 +57,7 @@ function findIconReferences(obj) {
 function validateAssetsDirectory() {
   const files = fs.readdirSync(ASSETS_DIR);
   if (files.length > MAX_COUNT_ASSETS) {
-    console.error(`Number of files in ${ASSETS_DIR} exceeds the maximum limit of ${MAX_COUNT_ASSETS}.`);
+    console.error(errorColor, `Number of files in ${ASSETS_DIR} exceeds the maximum limit of ${MAX_COUNT_ASSETS}.`);
     process.exit(1);
   }
 
@@ -63,12 +66,12 @@ function validateAssetsDirectory() {
     const filePath = path.join(ASSETS_DIR, file);
 
     if (!ASSET_TYPES.includes(ext)) {
-      console.error(`Unsupported file extension in ${ASSETS_DIR}: ${file}`);
+      console.error(errorColor, `Unsupported file extension in ${ASSETS_DIR}: ${file}`);
       process.exit(1);
     }
 
     if (getFileSize(filePath) > MAX_ASSET_FILE_SIZE) {
-      console.error(`File size of ${file} exceeds the maximum limit of ${MAX_ASSET_FILE_SIZE} bytes.`);
+      console.error(errorColor, `File size of ${file} exceeds the maximum limit of ${MAX_ASSET_FILE_SIZE} bytes.`);
       process.exit(1);
     }
   });
@@ -84,32 +87,32 @@ function validateRootDirectory() {
     return fs.statSync(filePath).isFile() && path.extname(file).toLowerCase() !== '.json';
   });
 
-  if (jsonFiles.length > MAX_COUNT_JSONS) {
-    console.error(`Number of JSON files in ${ROOT_DIR} exceeds the maximum limit of ${MAX_COUNT_JSONS}.`);
+  if (jsonFiles.length > MAX_COUNT_JSONS + 1) {
+    console.error(errorColor, `Number of JSON files in ${ROOT_DIR} exceeds the maximum limit of ${MAX_COUNT_JSONS + 1}.`);
     process.exit(1);
   }
 
   if (notJsonFiles.length > 0) {
-    console.error(`Unspoorted file extensions were found in ${ROOT_DIR}\nFiles: ${notJsonFiles.join(', ')}`);
+    console.error(errorColor, `Unsupported file extensions were found in ${ROOT_DIR}\nFiles: ${notJsonFiles.join(', ')}`);
     process.exit(1);
   }
 
   const productFile = jsonFiles.find(file => file.toLowerCase() === 'product.json');
   if (!productFile) {
-    console.error('product.json file is missing in the root directory.');
+    console.error(errorColor, 'product.json file is missing in the root directory.');
     process.exit(1);
   }
 
   jsonFiles.forEach(file => {
     const filePath = path.join(ROOT_DIR, file);
     if (getFileSize(filePath) > MAX_JSON_SIZE) {
-      console.error(`File size of ${file} exceeds the maximum limit of ${MAX_JSON_SIZE} bytes.`);
+      console.error(errorColor, `File size of ${file} exceeds the maximum limit of ${MAX_JSON_SIZE} bytes.`);
       process.exit(1);
     }
     if (file !== 'product.json') {
       const data = JSON.parse(fs.readFileSync(filePath));
       if (!validateItem(data)) {
-        console.error(`${file} data is invalid:`, validateItem.errors);
+        console.error(errorColor, `${file} data is invalid:`, validateItem.errors);
         process.exit(1);
       }
       itemDataMap.set(file, data);
@@ -133,7 +136,7 @@ function validateIconReferences() {
   // Validate if all referenced icons exist in the assets folder
   expectedIconsSet.forEach(icon => {
     if (!assetFilesSet.has(icon)) {
-      console.error(`Icon file ${icon} referenced in JSON data is missing in the assets folder.`);
+      console.error(errorColor, `Icon file ${icon} referenced in JSON data is missing in the assets folder.`);
       process.exit(1);
     }
   });
@@ -141,7 +144,7 @@ function validateIconReferences() {
   // Validate if there are any unexpected files in the assets folder
   assetFilesSet.forEach(file => {
     if (!expectedIconsSet.has(file)) {
-      console.error(`Unexpected file ${file} found in the assets folder.`);
+      console.error(errorColor, `Unexpected file ${file} found in the assets folder.`);
       process.exit(1);
     }
   });
