8fede351c3a0f8c256acebf4471a39462348d32d
Zaina Drawshy <zainadrawshy@microsoft.com>
2024-10-31 16:13:33 +0200

    tab on init action implemntation

--- Frontend/src/index.ui.tsx
+++ Frontend/src/index.ui.tsx
@@ -7,10 +7,27 @@ import { createWorkloadClient, InitParams } from '@ms-fabric/workload-client';
 
 import { fabricLightTheme } from "./theme";
 import { App } from "./App";
+import { convertGetItemResultToWorkloadItem } from "./utils";
+import { callItemGet } from "./controller/SampleWorkloadController";
+import { ItemPayload } from "./models/SampleWorkloadModel";
+import { DataWithId } from './models/SampleWorkloadModel';
 
 export async function initialize(params: InitParams) {
     const workloadClient = createWorkloadClient();
-
+    workloadClient.action.onAction(async function ({ action, data }) {
+        switch (action) {
+            case 'sample.tab.onInit':
+                const { id } = data as DataWithId;
+                const getItemResult = await callItemGet(
+                    id,
+                    workloadClient
+                );
+                const item = convertGetItemResultToWorkloadItem<ItemPayload>(getItemResult);
+                return {title: item.displayName};
+            default:
+                throw new Error('Unknown action received');
+        }
+    });
     const history = createBrowserHistory();
     workloadClient.navigation.onNavigate((route) => history.replace(route.targetUrl));
 
