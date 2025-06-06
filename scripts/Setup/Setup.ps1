ead2d24bde227c7166bc712af193f80035fd65cd
Gerd Saurer <gerd.saurer@microsoft.com>
2025-06-06 15:39:13 +0200

    Adding initial setup script

--- scripts/Setup/Setup.ps1
+++ scripts/Setup/Setup.ps1
@@ -1,6 +1,18 @@
 Write-Output "Setting up the environment..."
+$nugetExe = Read-Host "Provide the path to the Nuge executable"
+$env:Path += ";$nugetExe"
+
 Write-Output "Creating a new Entra Application..."
+& "$PSScriptRoot\CreateDevAADApp.ps1"
+
 Write-Output "Configuring the project..."
+Write-Output "Not implemented yet"
+
 Write-Output "Building the manifes..."
+& "$PSScriptRoot\..\Manifest\build-package.ps1" FERemote
+
 Write-Output "Starting the Frontend..."
+Write-Output "Not implemented yet"
+
 Write-Output "Starting the Dev-Gateway..."
+Write-Output "Not implemented yet"
