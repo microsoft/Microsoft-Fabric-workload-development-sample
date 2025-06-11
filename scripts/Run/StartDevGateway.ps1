e172fe8e528f426525ec740983abfefce6d83f46
Gerd Saurer <gerd.saurer@microsoft.com>
2025-06-11 15:20:19 +0200

    shell script to start the dev gateway

---
+++ scripts/Run/StartDevGateway.ps1
@@ -0,0 +1,4 @@
+$fileExe = $PSScriptRoot + "\..\..\tools\DevGateway\Microsoft.Fabric.Workload.DevGateway.exe"
+$CONFIGURATIONFILE = $PSScriptRoot + "\..\..\config\DevGateway\workload-dev-mode.json"
+Write-Host "Configurationfile used: $CONFIGURATIONFILE"
+& $fileExe -DevMode:LocalConfigFilePath $CONFIGURATIONFILE
