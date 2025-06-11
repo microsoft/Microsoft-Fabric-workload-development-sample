1c923a63eb8d9147ff22c80dc823480c08d500c9
Gerd Saurer <gerd.saurer@microsoft.com>
2025-06-11 15:22:30 +0200

    Adding deployment script to the sample

---
+++ scripts/Deployment/Deploy.ps1
@@ -0,0 +1,5 @@
+Write-Output "Deploying frontend to Azure..."
+Write-Output "Not implemented yet, please use the Azure Portal to deploy the frontend."
+
+Write-Output "Publishing Manifest to Fabric..."
+Write-Debug "not implemented yet, please use the Fabric Portal to publish the Manifest."
