d55901a1e540b142e97c2dad73dee96cec8bb313
Gerd Saurer <gerd.saurer@gmail.com>
2025-07-11 19:32:20 +0000

    fixing StartDevGateway.ps1 for devcontainer

--- scripts/Run/StartDevGateway.ps1
+++ scripts/Run/StartDevGateway.ps1
@@ -1,3 +1,7 @@
+param (
+    [boolean]$InteractiveLogin = $true
+)
+
 $fileExe = ""
 if($IsWindows) { 
     $fileExe = Join-Path $PSScriptRoot "..\..\tools\DevGateway\Microsoft.Fabric.Workload.DevGateway.exe"
@@ -9,14 +13,32 @@ $CONFIGURATIONFILE = Resolve-Path -Path (Join-Path $PSScriptRoot "..\..\config\D
 $CONFIGURATIONFILE = $CONFIGURATIONFILE.Path
 Write-Host "DevGateway used: $fileExe"
 Write-Host "Configuration xsfile used: $CONFIGURATIONFILE"
+
+$token = ""
+# For Codespaces, we can't use interactive login, so we need to use az login with device code
+# This is required to get the access token for the Fabric API
+if ($env:CODESPACES -eq "true" -or -not $InteractiveLogin) {
+    # Check if already logged in
+    $account = az account show 2>$null
+    if (-not $account) {
+        Write-Host "Not logged in. You ndeed to perform az login..." -ForegroundColor Red
+        az config set core.login_experience_v2=off | Out-Null
+        Write-Host "Please call the below command before starting the DevGateway again!"
+        Write-Host "az login -t <YOUR_FABRIC_TENANT_ID> --allow-no-subscriptions --use-device-code" -ForegroundColor Blue
+        exit 1
+    } else {
+        Write-Host "Already logged in."
+    }
+
+    $token = & az account get-access-token --scope https://analysis.windows.net/powerbi/api/.default --query accessToken -o tsv 
+}
+$config = Get-Content -Path $CONFIGURATIONFILE -Raw | ConvertFrom-Json 
+$manifestPackageFilePath = $config.ManifestPackageFilePath 
+$workspaceGuid = $config.WorkspaceGuid 
+$workloadEndpointURL = $config.WorkloadEndpointURL 
+
 if($IsWindows) { 
     & $fileExe -DevMode:LocalConfigFilePath $CONFIGURATIONFILE
-} else { 
-    #$token = az account get-access-token --scope https://analysis.windows.net/powerbi/api/.default --query accessToken -o tsv 
-    $token = "" 
-    $config = Get-Content -Path $CONFIGURATIONFILE -Raw | ConvertFrom-Json 
-    $manifestPackageFilePath = $config.ManifestPackageFilePath 
-    $workspaceGuid = $config.WorkspaceGuid 
-    $workloadEndpointURL = $config.WorkloadEndpointURL 
+} else {     
     & dotnet $fileExe -DevMode:UserAuthorizationToken $token -DevMode:ManifestPackageFilePath $manifestPackageFilePath -DevMode:WorkspaceGuid $workspaceGuid -DevMode:WorkloadEndpointUrl $workloadEndpointURL
 }
