3332ff969c5c0780f1dcd2c925732b0fafcc16dc
Gerd Saurer <gerd.saurer@microsoft.com>
2025-07-11 14:08:57 +0200

    Moving building the nuget from npm to The StartFrontend.ps1 Script and cleaning the StartDevGateway.ps1 script

--- scripts/Run/StartDevGateway.ps1
+++ scripts/Run/StartDevGateway.ps1
@@ -1,13 +1,3 @@
-$fileExe = Join-Path $PSScriptRoot "..\..\tools\DevGateway\Microsoft.Fabric.Workload.DevGateway.exe"
-$CONFIGURATIONFILE = Resolve-Path -Path (Join-Path $PSScriptRoot "..\..\config\DevGateway\workload-dev-mode.json")
-$CONFIGURATIONFILE = $CONFIGURATIONFILE.Path
-Write-Host "DevGateway used: $fileExe"
-Write-Host "Configuration xsfile used: $CONFIGURATIONFILE"
-
-& $fileExe -DevMode:LocalConfigFilePath $CONFIGURATIONFILE
-
-
-
 $fileExe = ""
 if($IsWindows) { 
     $fileExe = Join-Path $PSScriptRoot "..\..\tools\DevGateway\Microsoft.Fabric.Workload.DevGateway.exe"
--- scripts/Run/StartFrontend.ps1
+++ scripts/Run/StartFrontend.ps1
@@ -1,5 +1,27 @@
-Write-Output "Starting the Frontend ..."
+Write-Host "Building Nuget Package ..."
 
+################################################
+# Build the current nuget package
+################################################
+$nugetPath = Join-Path $PSScriptRoot "..\..\Frontend\node_modules\.bin\nuget.ps1"
+$nuspecPath = Join-Path $PSScriptRoot "..\..\config\Manifest\ManifestPackage.nuspec"
+$outputDir = Join-Path $PSScriptRoot "..\..\config\Manifest\"
+
+if($IsWindows){
+    & $nugetPath pack $nuspecPath -OutputDirectory $outputDir -Verbosity detailed
+} else {
+    # On Mac and Linux, we need to use pwsh to run the script
+    $pwshPath = "pwsh"
+    & $pwshPath $nugetPath pack $nuspecPath -OutputDirectory $outputDir -Verbosity detailed
+}
+
+Write-Host "Nuget package built successfully and saved to $outputDir"
+
+################################################
+# Starting the Frontend
+################################################
+Write-Host ""
+Write-Host "Starting the Frontend ..."
 try {
     $frontendDir = Join-Path $PSScriptRoot "..\..\Frontend"
     Push-Location $frontendDir
