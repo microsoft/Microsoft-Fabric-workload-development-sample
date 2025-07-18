2cf14f8173d5e99754f08e9be5a21fbbfc3102ee
Gerd Saurer <gerd.saurer@microsoft.com>
2025-07-18 08:23:01 +0200

    merging back dev/preview/wdkv2

--- Workload/app/constants.ts
+++ Workload/app/constants.ts
@@ -1,15 +1,6 @@
-import { CalculationOperator, CalculatorSampleItemDefinition } from "./samples/items/CalculatorSampleItem/CalculatorSampleItemModel";
-
 export enum EnvironmentConstants {
     FabricApiBaseUrl = "https://api.fabric.microsoft.com",
     OneLakeDFSBaseUrl= "https://onelake.dfs.fabric.microsoft.com",
-}
-
-export const defaultCalculatorSampleItemDefinition: CalculatorSampleItemDefinition = {
-    operand1: 0,
-    operand2: 0,
-    operator: CalculationOperator.Undefined,
-    lastResultFile: null
 }
 
 
